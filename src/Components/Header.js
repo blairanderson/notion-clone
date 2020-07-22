dd72c4327f35ed570fd863dba4e5c2ca8eeaa2ca
Blair Anderson <blair81@gmail.com>
2020-07-22 14:46:31 -0700

    add a link in the header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,11 +4,17 @@ const GITHUB = "https://github.com/blairanderson/notion-clone";
 const CODESANDBOX =
   "https://codesandbox.io/s/github/blairanderson/notion-clone";
 
+const headerLink = window.location.href.startsWith("https://codesandbox")
+  ? "https://codesandbox.io/s/github/blairanderson/notion-clone"
+  : "https://notionclone.netlify.app/";
+
 export default function Header(props) {
   return (
     <>
       <h1 style={{ marginBottom: 0, fontFamily: "sans-serif" }}>
-        React Notion Clone
+        <a style={{ color: "black" }} href={headerLink}>
+          React Notion Clone
+        </a>
       </h1>
       <div style={{ marginBottom: "10px" }}>
         <a href={GITHUB}>Github</a> / <a href={CODESANDBOX}>Codesandbox</a>
