b6113b04240bb3305fed283f9ec69de502257a57
Blair Anderson <blair81@gmail.com>
2020-07-24 11:47:36 -0700

    features

--- src/Components/Features.js
+++ src/Components/Features.js
@@ -2,18 +2,19 @@ import React from "react";
 
 const FEATURES = [
   "Drag and Drop Items",
-  "Nested Items",
-  "Rows are infinitely DEEP",
+  "Nested Items (with `maxDepth` configuration)",
   "'Enter' Key creates a new row",
   "'Backspace' key at beginning of Row should remove the row if empty",
   "'TAB' increase the depth of a row and children",
   "'SHIFT-TAB' decreases the depth of a row and children",
   "typing `[]` or `[x]` should convert the text row into a checkbox row",
-  "'Backspace' key at beginning of checkbox row converts back to a text row"
+  "Optional: PREFIX-Numbers are null/top/all",
+  "Optional: maxDepth to prevent infinitely deep nesting"
 ];
 
 const TODO = [
   "All Changes should be dastabase-backed to an API server thing",
+  "'Backspace' key at beginning of checkbox row converts back to a text row",
   "'Backspace' key at beginning of Row should merge current and previous rows",
   "'Enter' key will move text after cursor to new row, if cursor in middle of row.",
   "Should be able to drag/drop photos onto the page, and create new image-rows",
